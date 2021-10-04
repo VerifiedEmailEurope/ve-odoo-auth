6799a169cd859b3104a7740ab45ad9b1687139a4
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-10-04 19:18:06 +0530

    [ADD] Set auto_delete false on Freja eID mail template.

--- freja_eid_integration/__manifest__.py
+++ freja_eid_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Freja eID Integration',
-    'version': '14.0.1.0.3',
+    'version': '14.0.1.0.4',
     'author': 'Verified Email Europe AB',
     'maintainer': 'Verified Email Europe AB',
     'contributors': 'Hemangi Rupareliya, Verified Email Europe AB, Fredrik Arvas',
